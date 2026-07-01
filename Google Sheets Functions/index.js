4eb296610f10d8de40326145407ca7b4371bc5a6
Chad Harper <chadharper811@gmail.com>
2026-07-01 06:23:35 -0500

    finished puesdo code for now with all parts that i currently need, might add more later

--- Google Sheets Functions/index.js
+++ Google Sheets Functions/index.js
@@ -1,5 +1,7 @@
 // Will write out puesdo code to start and work into the real javascript from there
 
+
+// 1st function will organize current sheets all at once
 // Need a function that will work with google sheets 
 // Function should loop through all sheets in spreadsheet
 // Function should then loop through only columns F & G
@@ -8,3 +10,5 @@
 // function will ignore specific words that should be left in caps and loop through the rest changing to proper case
 // function could possibly rearrange content to match my organize method (might not work as intended too many mistakes to fix possibly)
 // function will then set the cell content
+
+// 2nd function will do same to cells but will trigger on edit and hopefully only run on the currently active sheet and not need to loop through all sheets
---
+++ Google Sheets Functions/index.test.js
@@ -0,0 +1,3 @@
+// will need tests to make sure the function output is as intended
+// will do multiple test for each part of the function separately 
+// will also test the function with all changes in last test
